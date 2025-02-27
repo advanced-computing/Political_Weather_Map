29dd23a998f3e780e113f5b6b8f276bd55aa7aba
Hiro <oiwa.hiroyuki1@gmail.com>
2025-02-27 00:36:35 -0500

    Update

--- main_page.py
+++ main_page.py
@@ -8,8 +8,8 @@ import os
 
 st.title('Political Weather Map by Country')
 st.write('Team: Charlotte Bacchetta, Samuel Bennett, Hiroyuki Oiwa')
-st.write("This project aims to assess how much a country's immmigration rates are associated with its "
-        "tone towards immigrants in news articles")
+st.write('This project aims to assess how much immmigration rates of a country'
+        ' are associated with its tone towards immigrants in news articles')
 
 # Function to load data
 def load_data(file_name):
@@ -18,14 +18,15 @@ def load_data(file_name):
 
 # Function to select columns
 def select_columns(df_article):
-    articles = pd.DataFrame()
-    articles['DateTime'] = pd.to_datetime(df_article['DateTime'])
-    articles['CountryCode'] = df_article['CountryCode']
-    articles['Title'] = df_article['Title']
-    articles['ContextualText'] = df_article['ContextualText']
-    articles['DocTone'] = df_article['DocTone']
-    articles['URL'] = df_article['URL']
-    return articles
+    columns = ['DateTime',
+               'CountryCode',
+               'Title',
+               'ContextualText',
+               'DocTone',
+               'URL'
+               ]
+    df_article['DateTime'] = pd.to_datetime(df_article['DateTime'])
+    return df_article[columns]
 
 # Function to convert original country code to alpha2 codes
 def code_mapping(articles, column):
@@ -41,7 +42,7 @@ def code_mapping(articles, column):
         iso_code = cols[2].text.strip()
         fips_to_iso[fips_code] = iso_code 
 
-    articles[column] = articles[column].replace(fips_to_iso)
+    articles.loc[:, column] = articles[column].replace(fips_to_iso)
     return articles
 
 # Function to convert alpha2 to alpha3 country codes
@@ -53,15 +54,33 @@ def convert_to_alpha3(alpha2_code):
 
 # Function to melt and clean data
 def melt_clean_data(df, value_name):
-    df_melted = df.melt(id_vars=['Country Code'], var_name='Year', value_name=value_name)
+    df_melted = df.melt(
+        id_vars=['Country Code'], 
+        var_name='Year', 
+        value_name=value_name
+        )
     df_melted['Year'] = pd.to_datetime(df_melted['Year'], format='%Y')
     return df_melted
 
 # Functon to plot map
 def plot_choropleth(data, value, title):
-    fig = px.choropleth(data, locations='Alpha3Code', color=value, hover_name='Alpha3Code', color_continuous_scale='Viridis')
-    fig.update_geos(showcoastlines=True, coastlinecolor='Black', showland=True, landcolor='white')
-    fig.update_layout(title=title, geo=dict(showframe=False, showcoastlines=True))
+    fig = px.choropleth(
+        data, 
+        locations='Alpha3Code', 
+        color=value, 
+        hover_name='Alpha3Code', 
+        color_continuous_scale='Viridis'
+        )
+    fig.update_geos(
+        showcoastlines=True, 
+        coastlinecolor='Black', 
+        showland=True, 
+        landcolor='white'
+        )
+    fig.update_layout(
+        title=title, 
+        geo=dict(showframe=False, showcoastlines=True)
+        )
     return fig
 
 # Load Data
@@ -72,7 +91,7 @@ df_pop = load_data('population.csv')
 # Prepare Data
 articles = select_columns(df_article)
 articles = code_mapping(articles, 'CountryCode')
-articles['Alpha3Code'] = articles['CountryCode'].apply(convert_to_alpha3)
+articles.loc[:, 'Alpha3Code'] = articles['CountryCode'].apply(convert_to_alpha3)
 imgs = melt_clean_data(df_img, 'Immigrants')
 pops = melt_clean_data(df_pop, 'Populations')
 
@@ -83,33 +102,68 @@ imgs_pops['Alpha3Code'] = imgs_pops['Country Code']
 
 # User input
 date_input = st.date_input('Select a Date', value=pd.to_datetime('2023-02-04'))
+st.write('We currently only have data for one date, 2023/02/04. '
+         'We will include all the dates in a later phase of the project')
 
 # Filter articles
-articles = articles[articles['ContextualText'].str.contains('immigra', case=False, na=False)]
+articles = articles[articles['ContextualText']
+                    .str.contains('immigra', case=False, na=False)]
 articles_date = articles[articles['DateTime'].dt.date == date_input]
-articles_country = articles_date.groupby('CountryCode')['DateTime'].count().reset_index(name='Count')
-articles_country['Alpha3Code'] = articles_country['CountryCode'].apply(convert_to_alpha3)
-articles_tone = articles_date.groupby('CountryCode')['DocTone'].mean().reset_index(name='Tone')
-articles_tone['Alpha3Code'] = articles_tone['CountryCode'].apply(convert_to_alpha3)
+articles_country = articles_date.groupby('CountryCode')['DateTime']\
+    .count().reset_index(name='Count')
+articles_country.loc[:, 'Alpha3Code'] = articles_country['CountryCode']\
+    .apply(convert_to_alpha3)
+articles_tone = articles_date.groupby('CountryCode')['DocTone']\
+    .mean().reset_index(name='Tone')
+articles_tone.loc[:, 'Alpha3Code'] = articles_tone['CountryCode']\
+    .apply(convert_to_alpha3)
 imgs_date = imgs_pops[imgs_pops['Year'].dt.year == date_input.year]
 
 # Scatter Plot Data
 scts = pd.merge(articles_tone, imgs_date, on='Alpha3Code')
 
 # Plotting
-fig_articles = plot_choropleth(articles_country, 'Count', 'Number of Articles about Immigrants by Country')
-fig_tones = plot_choropleth(articles_tone, 'Tone', 'Mean Tone toward Immigrants by Country')
-fig_imgs = plot_choropleth(imgs_date, 'Rate(%)', 'New Immigration Rate per Capita (%) by Country')
+fig_articles = plot_choropleth(
+    articles_country, 
+    'Count', 
+    'Number of Articles about Immigrants by Country')
+fig_tones = plot_choropleth(
+    articles_tone, 
+    'Tone', 
+    'Mean Tone toward Immigrants by Country')
+fig_imgs = plot_choropleth(
+    imgs_date, 
+    'Rate(%)', 
+    'New Immigration Rate per Capita (%) by Country')
 
 fig_scts = px.scatter(
-    scts, x='Rate(%)', y='Tone', text='Alpha3Code', title='Tone toward Immigrants vs. Immigration Rate per Capita(%) by Country',
-    hover_data={'Alpha3Code': True, 'Rate(%)': True, 'Tone': True}, trendline='ols', color_discrete_sequence=['black'], trendline_color_override='red'
+    scts, 
+    x='Rate(%)', 
+    y='Tone', 
+    text='Alpha3Code',
+    title='Tone toward Immigrants vs Immigration Rate per Capita(%) by State',
+    hover_data={'Alpha3Code': True, 'Rate(%)': True, 'Tone': True},
+    trendline='ols',
+    color_discrete_sequence=['black'],
+    trendline_color_override='red'
 )
 fig_scts.update_traces(textposition='top center')
 
 # Streamlit
-st.write('Articles Data Sample', articles.sample(5))
-st.plotly_chart(fig_articles)
-st.plotly_chart(fig_tones)
-st.plotly_chart(fig_imgs)
-st.plotly_chart(fig_scts)
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Main Page", "Second Page"])
+
+if page == "Main Page":
+    st.title('Main Page')
+    st.write('On this page we show data for articles on immigration on a map')
+    st.write('Articles Data Sample', articles.sample(5))
+    st.plotly_chart(fig_articles)
+    st.plotly_chart(fig_tones)
+else:
+    st.title('Immigrant Trend by Country')
+    st.write('On this page we show immigration rate per country on a map and '
+        'how that is associated with tone toward immigrants in a scatter plot')
+    st.write('Immigration Data Sample', imgs_pops.sample(5))
+    st.write('Data from World Bank. Rate(%) is Immigrants / Population')
+    st.plotly_chart(fig_imgs)
+    st.plotly_chart(fig_scts)
